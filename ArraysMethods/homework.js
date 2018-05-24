28f65f49e19a1784ead956ff0f6eb243180b07c9
SerheievMainacad <serheiev@mainacad.com>
2018-05-24 13:25:10 +0300

    ArraysMethodsHomeWork

---
+++ ArraysMethods/homework.js
@@ -0,0 +1,48 @@
+//tasks explaeined in commets below
+
+
+var task1 = 'I&am$the$Best$Front$end&developer$ever'
+// create array from string - ['I', 'am', 'the', 'best', 'Front', 'end', 'developer', 'ever'];
+
+var task2;
+// using array from previous task , join method and document.write - render each item of array in h2 tag
+
+//task 3
+var months = [
+				'December', 
+				'January', 
+				'February', 
+				'March', 
+				'April', 
+				'May', 
+				'June', 
+				'July', 
+				'August', 
+				'September', 
+				'October', 
+				'November'
+			];
+var yearSeasons = [];
+
+// create 4 different arrays which named Winter, Spring, Summer, Autumn whith using slice method; add those arrays in yearSeasons
+// show year Seasons in console
+//create a prompt window, which asked month name and after get result give alert which Season of year is it
+
+
+
+
+var task4 = [1,2,3,4,5,6,7,8,9];
+var task4Copy;
+//create a copy of task4 array with using push/unshift method; 
+
+
+var task5 = [2,22,654,652,578, 12354,1,2,6,5,4,344,665,225,1245,124];
+var task5Filtered
+//filter this array by using filter method'
+
+var task6 = [1,2,3,4,5,6,7,8,9];
+var result1,result2,result3;
+//result1 = summ of all array items
+//result2 = copy of array taks 6, each element newElement = oldElement**2
+//result3 = create a function which get array as a first paramet and some number as a second.
+//each element from result3 array increased by value of second parametr of function and function returns new array which 
